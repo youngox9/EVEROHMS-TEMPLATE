9d3537308f5c9c53a468c7f0652ae9b4983764cd
rex <youngox9@gmail.com>
2024-10-08 09:09:46 +0800

    add to template
    # Conflicts:
    #	package.json
    #	src/components/PivotTable.vue
    #	src/main.js
    #	src/utils/index.js

--- src/components/QRcode.vue
+++ src/components/QRcode.vue
@@ -1,39 +1,39 @@
 <template>
-  <img v-if="props.value" :src="qrcode" v-bind="{ ...$attrs, ...$props }" />
-
+  <img :src="qrcode" v-bind="{ ...$attrs, ...$props }" />
 </template>
 
 <script setup>
- import { useStore } from "vuex";
-//import { useState } from "@/utils";
-import { computed, ref, watch } from "vue";
-import { useRouter } from "vue-router";
-import { useQRCode } from "@vueuse/integrations/useQRCode";
+import { useStore } from 'vuex';
+import { computed, ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+import { useQRCode } from '@vueuse/integrations/useQRCode';
 
- const src = ref(null);
+const store = useStore();
+const router = useRouter();
 
 const props = defineProps({
-  value: {
+  modelValue: {
     type: String,
     default: () => {
-      return "";
+      return '';
     },
   },
 });
 
+const src = ref(props?.value);
 
-
-const qrcode = useQRCode(src || "1");
+const qrcode = useQRCode(src || '1');
 
 watch(
-  () => props.value,
+  () => props.modelValue,
   (val, prev) => {
-
-    src.value = val;
+    try {
+      src.value = val.replace(/\t/g, ' ');
+    } catch (e) {
+      console.log(e);
+      src.value = val;
+    }
   },
   { deep: true, immediate: true }
 );
-
-
-
 </script>
